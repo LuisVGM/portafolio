f04e6cb4d462cfab886b833603091a837fa0df6d
Luis Valentin <lvg.mx1@gmail.com>
2022-09-25 23:06:15 -0500

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,29 +1,27 @@
+const text = document.querySelector('.saludo');
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent ="";
 
-/*window.addEventListener('resize', function () {
-    
-    if(window.innerWidth <= '1024' || window.innerWidth >= '390'){
-        console.log(window.innerWidth);
-        document.querySelector("#home").classList.add('d-md-block');
-        document.querySelector("#about").classList.add('d-md-block');
-   }else{
-        document.querySelector("#home").classList.remove("d-dm-block");
-        document.querySelector("#about").classList.remove("d-dm-block");
+for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('animacion');
+    console.log(span.textContent);
+    char++
+    if (char === splitText.length) {
+        complete();
+        return;
     }
-});
-jQuery(document).ready(function($) {
-    var alterClass = function() {
-      var ww = document.body.clientWidth;
-      if (ww >= 390) {
-        $('#home').removeClass('d-md-block');
-        $('#about').removeClass('d-md-block');
-      } else if (ww <= 1000) {
-        $('#home').addClass('d-md-block');
-        $('#about').addClass('d-md-block');
-      };
-    };
-    $(window).resize(function(){
-      alterClass();
-    });
-    //Fire it when the page first loads:
-    alterClass();
-  });*/
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
