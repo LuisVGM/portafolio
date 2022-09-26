5bda9732cc824400f33c5b2ea4d2b8e77f3cbd58
Luis Valentin <lvg.mx1@gmail.com>
2022-09-26 01:36:14 -0500

    Add files via upload

--- js/name.js
+++ js/name.js
@@ -1,55 +1,57 @@
-function animar (){
-    const text = document.querySelector('.saludo');
+function animarName (){
+    const text = document.querySelector('.nombre');
     const strText = text.textContent;
-    const splitText = strText.split("");
+    const splitText = strText.split(",");
     text.textContent ="";
 
     for (let i = 0; i < splitText.length; i++) {
         text.innerHTML += "<span>" + splitText[i] + "</span>";
     }
-
+    
     let char = 0;
     let timer = setInterval(onTick, 50);
-
+    
     function onTick(){
         const span = text.querySelectorAll('span')[char];
-        span.classList.add('animacion');
+        span.classList.add('animacionName');
+        
         char++
         if (char === splitText.length) {
             complete();
             return;
         }
     }
-
+    
     function complete() {
         clearInterval(timer);
         timer = null;
     }
 }
 
-function regresar() {
-    const text = document.querySelector('.saludo');
+function regresarName() {
+    const text = document.querySelector('.nombre');
     const strText = text.textContent;
-    const splitText = strText.split("");
+    const splitText = strText.split(",");
     text.textContent ="";
 
     for (let i = 0; i < splitText.length; i++) {
         text.innerHTML += "<span>" + splitText[i] + "</span>";
     }
-
+    
     let char = 0;
     let timer = setInterval(onTick, 50);
-
+    
     function onTick(){
         const span = text.querySelectorAll('span')[char];
-        span.classList.remove('animacion');
+        span.classList.remove('animacionName');
+        
         char++
         if (char === splitText.length) {
             complete();
             return;
         }
     }
-
+    
     function complete() {
         clearInterval(timer);
         timer = null;
